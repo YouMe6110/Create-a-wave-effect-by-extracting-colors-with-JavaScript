98c6b1c550ffef1028cb0ffe60b0a7201c7cf605
YouMe6110 <ume0116@naver.com>
2022-07-18 00:42:06 +0900

    DAY 05

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import {Ripple} from './ripple.js';
+import {Dot} from './dot.js';
 
 class App {
     constructor() {
@@ -6,12 +7,19 @@ class App {
         document.body.appendChild(this.canvas);
         this.ctx = this.canvas.getContext('2d');
 
+        this.tmpCanvas = document.createElement('canvas');
+        this.tmpCtx = this.tmpCanvas.getContext('2d');
+
         this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
         this.ripple = new Ripple();
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
+
+        this.radius = 10;
+        this.pixelSize = 30;
+        this.dots = [];
 
         this.isLoaded = false;
         this.imgPos = {
@@ -40,6 +48,9 @@ class App {
         this.canvas.width = this.stageWidth * this.pixelRatio;
         this.canvas.height = this.stageHeight * this.pixelRatio;
         this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+        this.tmpCanvas.width = this.stageWidth;
+        this.tmpCanvas.height = this.stageHeight;
 
         this.ripple.resize(this.stageWidth, this.stageHeight);
 
@@ -72,6 +83,20 @@ class App {
             this.imgPos.x, this.imgPos.y,
             this,this.imgPos.width, this.imgPos.height,
         );
+
+        this.ctx.drawImage(
+            this.image,
+            0, 0,
+            this.image.width, this.image.height,
+            this.imgPos.x, this.imgPos.y,
+            this,this.imgPos.width, this.imgPos.height,
+        );
+
+        this.drawDots();
+    }
+
+    drawDots() {
+        this.dots = [];
     }
 
     animate() {
